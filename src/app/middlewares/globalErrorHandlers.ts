bab6ffa37c8b3cc357b9bfe45a14ae535045f2b0
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-06-03 10:40:36 +0600

    made handler for keep zod error format same in global handler

--- src/app/middlewares/globalErrorHandlers.ts
+++ src/app/middlewares/globalErrorHandlers.ts
@@ -1,21 +1,51 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
-const globalErrorHandlers = (
-    err: any,
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Something went wrong';
+
+const globalErrorHandlers: ErrorRequestHandler = (err, req, res, next) => {
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Something went wrong';
+    let errorSources: TErrorSource = [
+        {
+            path: '',
+            message: 'Something went wrong'
+        }
+    ]
+
+    const handleZodError = (err: ZodError) => {
+        const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+            return {
+                path: issue?.path[issue.path.length - 1],
+                message: issue?.message
+            }
+        })
+        const statusCode = 400
+
+        return {
+            statusCode,
+            message: 'Validation Error',
+            errorSources,
+            stack: config.NODE_DEV === 'development' ? err?.stack : null
+        }
+    }
+
+    if (err instanceof ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError?.statusCode
+        message = simplifiedError?.message
+        errorSources = simplifiedError?.errorSources
+    }
 
     return res.status(statusCode).json({
         success: false,
         message,
-        error: err
+        errorSources,
+        // error: err
     });
 }
 
