63b51afeb530d919ae7795c1731e4d14d71d9969
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-06-09 18:37:03 +0600

    modified the validtion for faculty

--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -13,44 +13,48 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
         academicFaculty,
         academicDepartment,
         course,
-        faculty
+        section,
+        faculty,
+        days,
+        startTime,
+        endTime,
     } = payload
 
     // check if the semester registration id is exists!
     const isSemesterRegistrationExists = await SemesterRegistration
         .findById(semesterRegistration)
 
-    if(!isSemesterRegistrationExists){
+    if (!isSemesterRegistrationExists) {
         throw new AppError(httpStatus.NOT_FOUND, 'Semester registration not found!')
     }
 
     const academicSemester = isSemesterRegistrationExists.academicSemester
 
-     // check if the academic faculty id is exists!
+    // check if the academic faculty id is exists!
     const isAcademicFacultyExists = await AcademicFaculty
         .findById(academicFaculty)
 
     if (!isAcademicFacultyExists) {
         throw new AppError(httpStatus.NOT_FOUND, 'Academic Faculty is not found!')
     }
-    
-     // check if the academic faculty id is exists!
+
+    // check if the academic faculty id is exists!
     const isAcademicDepartmentExists = await AcademicDepartment
         .findById(academicDepartment)
 
     if (!isAcademicDepartmentExists) {
         throw new AppError(httpStatus.NOT_FOUND, 'Academic Department is not found!')
     }
-    
-     // check if the academic department id is exists!
+
+    // check if the academic department id is exists!
     const isCourseExists = await Course
         .findById(course)
 
     if (!isCourseExists) {
         throw new AppError(httpStatus.NOT_FOUND, 'Course is not found!')
     }
-    
-     // check if the academic department id is exists!
+
+    // check if the academic department id is exists!
     const isFacultyExists = await Course
         .findById(faculty)
 
@@ -58,7 +62,51 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
         throw new AppError(httpStatus.NOT_FOUND, 'Faculty is not found!')
     }
 
-    const result = await OfferedCourse.create({...payload,academicSemester});
+    //check if the department is belong to the faculty
+    const isDepartmentBelongToFaculty = await AcademicDepartment.findOne({
+        _id: academicDepartment,
+        academicFaculty,
+    })
+
+    if (!isDepartmentBelongToFaculty) {
+        throw new AppError(httpStatus.BAD_REQUEST, `${isAcademicDepartmentExists} does not belong to this ${isAcademicFacultyExists}`)
+    }
+
+    const isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection = await OfferedCourse.findOne({
+        semesterRegistration,
+        course,
+        section
+    })
+
+    if (isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection) {
+        throw new AppError(httpStatus.BAD_REQUEST, `Offered course with same section already exist`)
+    }
+
+    //get the schedule of the faculties
+    const assignSchedules = await OfferedCourse.find({
+        semesterRegistration,
+        faculty,
+        days: { $in: days }
+    }).select('days startTime endTime')
+
+    const newSchedule = {
+        days,
+        startTime,
+        endTime
+    }
+
+    assignSchedules.forEach((schedule) => {
+        const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`)
+        const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`)
+        const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`)
+        const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`)
+
+        if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+            throw new AppError(httpStatus.CONFLICT, `This faculty is not available that time`)
+        }
+    })
+
+    const result = await OfferedCourse.create({ ...payload, academicSemester });
     return result
 }
 
